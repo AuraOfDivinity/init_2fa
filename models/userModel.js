aa4a2672bc7864affbeb59219e413183bf922f1f
Asel <peirisasel373@Gmail.com>
2021-06-27 23:55:45 +0530

    Verify OTP endpoint + sending the OTP using twilio.

--- models/userModel.js
+++ models/userModel.js
@@ -2,28 +2,32 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema(
     {
-        name: { 
-            type: String, 
+        name: {
+            type: String,
             required: 'Name is required'
-        }, 
+        },
         email: {
             type: String,
             require: 'Email is required',
             unique: true
         },
-        password: { 
-            type: String, 
+        password: {
+            type: String,
             required: 'Password is required'
         },
-        phone: { 
-            type: String, 
+        phone: {
+            type: String,
             required: 'Phone Number is required'
-        },  
+        },
+        otp: {
+            type: String,
+            required: 'User OTP is required'
+        },
         isVerified: {
             type: Boolean,
             default: false
         }
-    }, 
+    },
     {
         timestamps: true
     }
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -7,26 +7,69 @@ const userRouter = express.Router();
 userRouter.post('/register', async (req, res, next) => {
     const { name, email, phone, password } = req.body
 
+    const otpNumber = Math.floor(100000 + Math.random() * 900000)
+
     //create a new user 
     const user = new User({
         name,
         email,
         password: bcrypt.hashSync(password, 8),
-        phone
+        phone,
+
+        // Generating a random number of 6 digits for the OTP
+        otp: otpNumber
     });
     // save new user in db
     const createdUser = await user.save();
 
+    // Send the otp number to user's phone using twilio programmable messaging
+    const accountSid = process.env.TWILIO_ACCOUNT_SID;
+    const authToken = process.env.TWILIO_AUTH_TOKEN;
+    const client = require('twilio')(accountSid, authToken);
+
+    client.messages
+        .create({ body: `Your account verification OTP is: ${otpNumber}`, from: '+18325583432', to: `${phone}` })
+        .then(message => console.log(message.sid)).catch(e => {
+            console.log(e)
+        });
+
     // send user obj back
     res.send({
-        _id: createdUser._id,
-        name: createdUser.name,
-        email: createdUser.email,
-        phone: createdUser.phone,
-        isVerified: createdUser.isVerified,
-        token: generateToken(createdUser)
-
+        message: "Success. An OTP has been sent to your phone number. Please verify it.",
+        data: {
+            _id: createdUser._id,
+            name: createdUser.name,
+            email: createdUser.email,
+            phone: createdUser.phone,
+            isVerified: createdUser.isVerified,
+            token: generateToken(createdUser)
+        }
     });
+})
+
+userRouter.post('/verifyOtp', async (req, res, next) => {
+    const { otp, phone } = req.body;
+
+    // Checking if a user exists with given phone number
+    const user = await User.findOne({ phone });
+    let updatedUser;
+
+    if (user && user.otp == otp) {
+        user.isVerified = true;
+        updatedUser = await user.save();
+
+        res.send({
+            message: "User successfully verified. You can now access protected resources using the provided jwt token",
+            data: {
+                _id: updatedUser._id,
+                name: updatedUser.name,
+                email: updatedUser.email,
+                isVerified: updatedUser.isVerified,
+                token: generateToken(updatedUser)
+            }
+        })
+    }
+    res.status(400).send({ message: 'No user found with the provided phone number' });
 })
 
 module.exports = userRouter;
